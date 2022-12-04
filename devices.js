9b8e870ef00f89c8a673eb5e4a66ee810e3c0c8d
Jayme Frantz <hello@jaymefrantz.com>
2022-12-04 15:07:10 -0500

    parsing json on ws message

--- devices.js
+++ devices.js
@@ -1,16 +1,27 @@
 import { WebSocketServer } from 'ws'
 
+/*
+  devices = {
+  "artbox": {
+    red, green, blue, name, isOn
+  }
+}
+*/
+
 export default function initWebsocket(server) {
-  console.log("initing websocket?");
   const wss = new WebSocketServer({ server });
   let devices = {}
 
   wss.on("connection", (ws) => {
-    console.log("i got a connection?");
-    ws.on("message", (data) => {
-      console.log('received: %s', data);
+    //console.log("i got a connection?");
+    ws.on("message", string => {
+      const json = JSON.parse(string)
+      console.log(json);
+      //devices[json.device] = {...json.data, timestamp: new Date()}
+      //send back devices to all other clients?
+
     });
 
-    ws.send('hello from node server...?');
+    //ws.send('hello from node server...?');
   });
 }
